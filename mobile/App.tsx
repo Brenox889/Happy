6e8758843955d2632d5a1089526099b116e7f07b
brenox889 <brenomendes889@hotmail.com>
2020-10-16 00:33:51 -0300

    feat: Add Routes to App

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, {Marker,Callout, PROVIDER_GOOGLE} from 'react-native-maps'
+import {useFonts} from 'expo-font'
+import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito'
+
+import mapMarker from './src/images/map-marker.png'
+
+import Routes from './src/routes'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+  if(!fontsLoaded){
+    return null
+  }
+  return (
+    <Routes />
+  );
+}
+
